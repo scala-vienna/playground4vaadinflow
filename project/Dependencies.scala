54499901caff3e08e786c80fc55348921c5c28ad
Otto Ringhofer <otto.ringhofer@gmail.com>
2018-06-19 23:20:08 +0200

    implement ClockDemo

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,18 +5,22 @@ object Dependencies {
   val vaadinVersion = "10.0.0.rc5" // vaadinVersion auch in reference.conf nachziehen
   val servletapiVersion = "3.1.0"
   val slf4jVersion = "1.7.25"
+  val akkaVersion = "2.5.13"
 
   val vaadinOrg = "com.vaadin"
   val slf4jOrg = "org.slf4j"
+  val akkaOrg = "com.typesafe.akka"
 
   val servletApi = "javax.servlet" % "javax.servlet-api" % servletapiVersion % "provided"
   val slf4jSimple = slf4jOrg % "slf4j-simple" % slf4jVersion
   val vaadinCore = vaadinOrg % "vaadin-core" % vaadinVersion
+  val akkaActor = akkaOrg %% "akka-actor" % akkaVersion
 
   val codeDeps = Seq(
     servletApi,
     slf4jSimple,
-    vaadinCore
+    vaadinCore,
+    akkaActor
   )
 
   // Vaadin 10 Flow has problems with default jetty in plugin :-(
